dc21d16830ae75abb486ae9c31b62f9ce326c1ef
Mickey <mickeyc11111@gmail.com>
2022-05-20 20:22:20 -0400

    commit tree 71ca817ccc4fda64f481e48979e98d2008a051fc

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let newArray = [];
+      for (let j = 0; j < array.length; j++) {
+        let message = `Thank you, ${array[j]}, for the wonderful ${event} gift!`;
+        newArray.push(message);
+      }
+    return newArray;
+  }
+
+  function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
